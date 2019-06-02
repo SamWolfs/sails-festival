69e2fab2a9397fb7b12cbc96141854bd74b1353e
Sam Wolfs <be.samwolfs@gmail.com>
2019-06-02 14:51:11 +0200

    General Styling, HTML Main, Correct playlists

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
-import './App.scss';
-import { Home } from './pages/Home';
-import { Lineup } from './pages/Lineup';
-import { Layout } from './components/Layout';
-import { Switch, Route } from 'react-router';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab, faTwitter, faFacebookF, faInstagram, faYoutube, faSpotify } from '@fortawesome/free-brands-svg-icons'
-import { far, faEnvelope, faMap } from '@fortawesome/free-regular-svg-icons';
+import React from "react";
+import "./App.scss";
+import { Home } from "./pages/Home";
+import { Lineup } from "./pages/Lineup";
+import { Layout } from "./components/Layout";
+import { Switch, Route } from "react-router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  fab,
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+  faYoutube,
+  faSpotify
+} from "@fortawesome/free-brands-svg-icons";
+import { far, faEnvelope, faMap } from "@fortawesome/free-regular-svg-icons";
 
-library.add(fab, far, faTwitter, faFacebookF, faInstagram, faYoutube, faSpotify, faEnvelope, faMap);
+library.add(
+  fab,
+  far,
+  faTwitter,
+  faFacebookF,
+  faInstagram,
+  faYoutube,
+  faSpotify,
+  faEnvelope,
+  faMap
+);
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-    <Layout>
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/lineup" component={Lineup} />
-        <Route path="/" component={Home} /> 
-      </Switch>
-    </Layout>
-    </div>
+    <>
+      <div className="Background" />
+      <div className="App">
+        <Layout>
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/lineup" component={Lineup} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Layout>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
