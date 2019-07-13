92fad1b5f3553e160f771e227be7ef2ede164600
Sam Wolfs <be.samwolfs@gmail.com>
2019-07-13 16:24:39 +0200

    Fix redirects

--- src/components/buttons/FAwesomeButton.tsx
+++ src/components/buttons/FAwesomeButton.tsx
@@ -6,7 +6,7 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 export const FAwesomeButton: React.FC<FAwesomeButtonProps> = (button: FAwesomeButtonProps) => {
   return (
     <div className={styles.container}>
-        <a href={button.link} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={button.icon}/></a>
+        <a href={button.link}><FontAwesomeIcon icon={button.icon}/></a>
     </div>
   );
 };
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,6 +3,7 @@ import logo from "../../assets/images/sails-logo.png";
 import styles from './Footer.module.scss';
 import { FAwesomeButton } from "../buttons/FAwesomeButton";
 import { SocialPlatform, SocialButtonProps, SocialButton } from "../buttons/SocialButton";
+import { NavLink } from "react-router-dom";
 
 export const Footer = () => {
   const socialPlatforms: SocialButtonProps[] = [
@@ -19,7 +20,7 @@ export const Footer = () => {
           })}
         </div>
         <div className="level-item">
-          <a href="/home"><img className={styles.logo} src={logo} alt="sails-logo" /></a>
+          <NavLink to="/home"><img className={styles.logo} src={logo} alt="sails-logo" /></NavLink>
         </div>
         <div className="level-item">
           <FAwesomeButton link="mailto:organisatie@sailsfestival.be" icon={['far', 'envelope']} />
